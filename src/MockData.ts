decb77a63bcc0bca711981992c332844199af650
Kaue Portella <portellakaue@gmail.com>
2024-10-02 23:35:09 -0300

    retirado arquivo não utilizado.

--- src/MockData.ts
+++
@@ -1,8 +0,0 @@
-import { User } from './User';
-
-export const prevUsers: User[] = [
-    { matricula: '1', nome: 'John Doe', email: 'john.doe@example.com', perfil: 'Técnico de Manutenção', ativo: false },
-    { matricula: '2', nome: 'Jane Smith', email: 'jane.smith@example.com', perfil: 'Marketing', ativo: true },
-    { matricula: '3', nome: 'Alice Johnson', email: 'alice.johnson@example.com', perfil: 'Administração', ativo: true },
-    { matricula: '4', nome: 'Bob Williams', email: 'bob.williams@example.com', perfil: 'Gerente', ativo: false },
-];
